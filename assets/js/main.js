4bc59bd6b5d2e17e50068a069004c65bf2cf17b1
Rishi Mishra <rm43084@gmail.com>
2024-02-20 19:28:55 +0530

    Images Inserted

--- assets/js/main.js
+++ assets/js/main.js
@@ -255,16 +255,16 @@
   new Swiper('.testimonials-slider', {
     speed: 600,
     loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
+    // autoplay: {
+    //   delay: 5000,
+    //   disableOnInteraction: false
+    // },
+    // slidesPerView: 'auto',
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   type: 'bullets',
+    //   clickable: true
+    // },
     breakpoints: {
       320: {
         slidesPerView: 1,
@@ -272,7 +272,7 @@
       },
 
       1200: {
-        slidesPerView: 3,
+        slidesPerView: 2,
         spaceBetween: 20
       }
     }
